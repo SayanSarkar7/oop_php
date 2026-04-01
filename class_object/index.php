06548bc5e478f455764525fb2390683e7a452d91
SayanSarkar7 <sarkarsayan602@gmail.com>
2026-04-02 00:57:55 +0530

    updated class and object file

--- class_object/index.php
+++ class_object/index.php
@@ -1,4 +1,24 @@
 <?php
 
-echo "hello sayan";
+class Car{
+    //properties
+    public $name="Car name";
+
+    //methods(function)
+    public function getCarName(){
+        return $this->name;
+    }
+    public function setCarName($car_name){
+        $this->name=$car_name;
+    }
+}
+
+$bmw=new Car();
+$bmw->name="BMW x5 series";
+echo $bmw->getCarName();
+
+$marcedes=new Car();
+$marcedes->setCarName("Marcedes");
+// $bmw->name="Marcedes benz series";
+echo $marcedes->getCarName();
 ?>
