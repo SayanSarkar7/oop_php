acbfb2e6a152a8f5e72132208504410910304b5e
SayanSarkar7 <sarkarsayan602@gmail.com>
2026-04-02 12:59:42 +0530

    updated constructor and destructor

--- class_object/index.php
+++ class_object/index.php
@@ -2,23 +2,30 @@
 
 class Car{
     //properties
-    public $name="Car name";
+    public $name;
 
+    public function __construct($car_name){
+        $this->name=$car_name;
+        echo "inside the constructor <br/>";
+    }
+    
     //methods(function)
     public function getCarName(){
         return $this->name;
     }
-    public function setCarName($car_name){
-        $this->name=$car_name;
+    public function __destruct()
+    {
+        echo "inside the destrutor<br/>";
     }
 }
 
-$bmw=new Car();
-$bmw->name="BMW x5 series";
-echo $bmw->getCarName();
+$bmw=new Car("BMW x5 series");
+echo "object ".$bmw->name." is created<br/>";
+echo $bmw->getCarName()." car <br/>";
+unset($bmw);
 
-$marcedes=new Car();
-$marcedes->setCarName("Marcedes");
+$marcedes=new Car("Marcedes Benz");
 // $bmw->name="Marcedes benz series";
-echo $marcedes->getCarName();
+echo "object ".$marcedes->name." is created<br/>";
+echo $marcedes->getCarName()." car <br/>";
 ?>
